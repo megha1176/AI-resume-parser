2c0bae7607163d325e5ec00560e13f26554cc8eb
megha0309 <megha@privme.com>
2025-05-13 10:03:03 +0530

    add

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
-
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
-
-if __name__ == '__main__': 
-    app.run(debug=True) 
+    return "Hello World!"
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
